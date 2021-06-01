d4aa7364b5a70698a424eea9ba1b4536411d3756
Esperlluette <janl.mannessier@gmail.com>
2021-06-01 11:53:33 +0000

    début du projet

---
+++ kalista.com/public/index.php
@@ -0,0 +1,6 @@
+<?php
+/* initialisation des fichiers TWIG */
+require_once '../lib/vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('../src/vue/');
+$twig = $twig = new \Twig\Environment($loader, []);
+echo 'Bienvenue sur mon site';
